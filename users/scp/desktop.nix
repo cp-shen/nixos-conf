0069c43c117cf10018d8facd20b06a9361ffda9e
scp <scp@archlinuxscp.xyz>
2022-04-15 01:07:13 +0800

    update gtk and qt setting

--- users/scp/desktop.nix
+++ users/scp/desktop.nix
@@ -12,16 +12,21 @@
   gtk = {
     enable = true;
     iconTheme = {
-      package = pkgs.numix-icon-theme;
-      name = "Numix";
+      #package = pkgs.numix-icon-theme;
+      #name = "Numix";
+      package = pkgs.gnome.adwaita-icon-theme;
+      name = "Adwaita";
     };
     theme = {
-      package = pkgs.numix-gtk-theme;
-      name = "Numix";
+      #package = pkgs.numix-gtk-theme;
+      #name = "Numix";
+      package = pkgs.gnome.gnome-themes-extra;
+      name = "Adwaita-dark";
     };
   };
   qt = {
     enable = true;
+    platformTheme = "gnome";
     style = {
       package = pkgs.adwaita-qt;
       name = "adwaita-dark";
