74a696199eea91ee2b8e0ba1c0df2fac6d45d8d9
scp <scp@archlinuxscp.xyz>
2022-10-19 14:28:51 +0800

    add vaapi config

--- system/xserver.nix
+++ system/xserver.nix
@@ -12,4 +12,23 @@
 
     displayManager.startx.enable = true;
   };
+
+  services.xserver.videoDrivers = [ "modesetting" ];
+  # services.xserver.useGlamor = true;
+  # services.xserver.videoDrivers = [ "intel" ];
+  # services.xserver.deviceSection = ''
+  #   Option "DRI" "2"
+  #   Option "TearFree" "true"
+  # '';
+
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-ocl
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
 }
