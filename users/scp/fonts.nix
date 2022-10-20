a9f324d113cbd9156bc6985d31d6df6de2c75648
scp <scp@archlinuxscp.xyz>
2022-10-21 00:07:07 +0800

    fonts

--- users/scp/fonts.nix
+++ users/scp/fonts.nix
@@ -6,14 +6,26 @@
   home.packages = with pkgs; [
     # nerd patched fonts
     (pkgs.nerdfonts.override {
-        fonts = [ "FiraCode" "Ubuntu" "CascadiaCode" "Hack"]; })
+        fonts = [ "FiraCode" "CascadiaCode" "Hack" "Ubuntu"]; })
 
     # programming fonts
-    fira-code
-    fira-code-symbols
-    hack-font
-    dejavu_fonts
-    cascadia-code
+        fira-code fira-code-symbols
+        # chars: good-looking but sharp (has serif) (like 'g', 'r')
+        # height: medium line spacing
+        # (44 lines cascadia-code/hack -> 42 lines fira code -> 39 lines jb mono)
+        # ligaratures: yes, italics: no, zero: slashed
+        cascadia-code
+        # chars: good-looking, not as sharp as fira code
+        # height: small line spacing
+        # ligaratures: yes, italics: yes, zero: dotted
+        jetbrains-mono
+        # chars: really smooth and rounded
+        # height: large line spacing
+        # ligaratures: yes, italics: yes, zero: dotted
+        hack-font
+        # chars: also smooth and rounded
+        # height: small line spacing
+        # ligaratures: no, italics: yes, zero: dotted
 
     # other fonts
     noto-fonts
