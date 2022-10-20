8e111c0291af040dce52a514ea8f3f3fa8d29bc6
scp <scp@archlinuxscp.xyz>
2022-10-20 17:58:12 +0800

    add fonts

--- users/scp/fonts.nix
+++ users/scp/fonts.nix
@@ -4,14 +4,21 @@
   fonts.fontconfig.enable = true;
 
   home.packages = with pkgs; [
+    # nerd patched fonts
     (pkgs.nerdfonts.override { fonts = [ "FiraCode" "Ubuntu" ]; })
+
+    # programming fonts
     fira-code
     fira-code-symbols
-    font-awesome
-    material-icons
+    hack-font
+    dejavu_fonts
+
+    # other fonts
     noto-fonts
     noto-fonts-cjk
     noto-fonts-emoji
+    font-awesome
+    material-icons
     ubuntu_font_family
   ];
 }
