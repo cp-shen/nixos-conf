70ef821d2fcd3675019af862a82789ef06dcb866
scp <scp@archlinuxscp.xyz>
2022-11-07 03:37:13 +0800

    set default applications

--- users/scp/misc.nix
+++ users/scp/misc.nix
@@ -1,5 +1,17 @@
 { config, pkgs, ... }:
 
 {
-  # TODO
+  xdg.mime.enable = true;
+  xdg.mimeApps = {
+    enable = true;
+
+    defaultApplications = {
+      "inode/directory" = [ "firefox.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/about" = [ "firefox.desktop" ];
+      "x-scheme-handler/unknown" = [ "firefox.desktop" ];
+    };
+  };
 }
