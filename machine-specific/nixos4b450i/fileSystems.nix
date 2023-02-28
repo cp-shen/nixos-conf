c7b5743ce8e5929123f0c0c43484e9ea60eec717
scp <scp@archlinuxscp.xyz>
2023-03-01 03:07:41 +0800

    add swap device

--- machine-specific/nixos4b450i/fileSystems.nix
+++ machine-specific/nixos4b450i/fileSystems.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  swapDevices = [
+    { device = "/dev/sda4"; }
+  ];
+
   fileSystems = {
 
     "/" = {
--- machine-specific/nixos4b450i/hardware-configuration.nix
+++ machine-specific/nixos4b450i/hardware-configuration.nix
@@ -13,7 +13,7 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  swapDevices = [ ];
+  # swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
