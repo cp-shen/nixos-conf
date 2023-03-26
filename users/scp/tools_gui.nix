9b3ab401f4597f9231aa73367fe95740d825b3af
scp <scp@archlinuxscp.xyz>
2023-03-26 11:11:38 +0800

    formatting

--- users/scp/tools_gui.nix
+++ users/scp/tools_gui.nix
@@ -62,15 +62,15 @@
       kb-clear-line = "Control+w";
       kb-cancel = "Escape,Super+Delete,Super+Tab,Control+g";
 
-      kb-select-1  = "Control+1";
-      kb-select-2  = "Control+2";
-      kb-select-3  = "Control+3";
-      kb-select-4  = "Control+4";
-      kb-select-5  = "Control+5";
-      kb-select-6  = "Control+6";
-      kb-select-7  = "Control+7";
-      kb-select-8  = "Control+8";
-      kb-select-9  = "Control+9";
+      kb-select-1 = "Control+1";
+      kb-select-2 = "Control+2";
+      kb-select-3 = "Control+3";
+      kb-select-4 = "Control+4";
+      kb-select-5 = "Control+5";
+      kb-select-6 = "Control+6";
+      kb-select-7 = "Control+7";
+      kb-select-8 = "Control+8";
+      kb-select-9 = "Control+9";
       kb-select-10 = "Control+0";
     };
   };
