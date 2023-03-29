cc8483999b811bb45630650b7f0f386ab726692b
scp <scp@archlinuxscp.xyz>
2023-03-29 19:02:49 +0800

    update substituters

--- system/misc.nix
+++ system/misc.nix
@@ -17,12 +17,14 @@
   nix = {
     settings = {
       substituters = [
-        "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+        # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
         "https://nix-community.cachix.org"
         "https://cache.nixos.org/"
+        "https://cache.iog.io"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       ];
     };
     package = pkgs.nixFlakes;
