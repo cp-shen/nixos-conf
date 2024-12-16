e0e6db78a0f49a1ed283511bf348205fd5e5d624
cp-shen <cp_shen@outlook.com>
2024-12-16 21:04:07 +0800

    update network DHCP config

--- system/networking.nix
+++ system/networking.nix
@@ -2,11 +2,12 @@
 
 {
   networking = {
+    useDHCP = false; # DHCP should be handled by networkmanager
     networkmanager.enable = true;
+
     proxy.default = "http://127.0.0.1:7890/";
     proxy.noProxy = "127.0.0.1,localhost,*.lan";
   
-    useDHCP = true;
     domain = "lan";
 
     firewall.enable = false;
