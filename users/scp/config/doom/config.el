d0e341b2d34fc8a9b2a7926d3a3f3d6cdde78cb9
scp <scp@archlinuxscp.xyz>
2024-12-16 19:32:32 +0800

    update emacs font settings

--- users/scp/config/doom/config.el
+++ users/scp/config/doom/config.el
@@ -2,6 +2,7 @@
       user-mail-address "scp@archlinuxscp.dev")
 
 (setq doom-font (font-spec
+                 ;; :family "CaskaydiaCove Nerd Font"
                  :family "JetBrainsMono Nerd Font"
                  :size 18 :weight 'normal))
 
