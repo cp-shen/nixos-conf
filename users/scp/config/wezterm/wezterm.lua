a57d6f3f540204240bd8c41fcfdd12e6ca7bfb0b
cp-shen <cp_shen@outlook.com>
2025-11-25 22:45:07 +0800

    update WezTerm config

--- users/scp/config/wezterm/wezterm.lua
+++ users/scp/config/wezterm/wezterm.lua
@@ -1,17 +1,22 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.color_scheme = 'DoomOne'
+config.color_scheme = 'Spacegray Eighties (Gogh)'
 
-config.font_size = 13
+config.font_size = 14
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 
+config.font = wezterm.font_with_fallback {
+  'JetBrains Mono',
+  -- 'Noto Sans CJK SC',
+}
+
 config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 
 wezterm.on("window-config-reloaded", function(window, pane)
